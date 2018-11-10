70048927445cc09550885e01e7f38c1d54821426
whitefury01 <white.pury1114@gmail.com>
2018-11-09 23:30:14 -0500

    revamped to make a quick blog reader for Finding Rubies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Used Open Brewery DB Api https://www.openbrewerydb.org/
+Used WordPress Rest API to pull in blogposts from FindingRubies.com
 
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md).
--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
-    this.state = {
-      brewery: []
-    };
+    this.state={
+      blogs: [],
+    }
   }
 
+// Fetch WordPress API blog posts
+  componentDidMount() {
+    let API = "http://findingrubies.com/wp-json/wp/v2/posts";
+    fetch(API)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({blogs: response
+        })
+      })
+  }
+
+// Render blog title and blog post to DOM
   render() {
+    let blogs = this.state.blogs.map((blogs, index) => {
+      return (
+        <div key={index}>
+          <h2 className="title">{blogs.title.rendered}</h2>
+          <div dangerouslySetInnerHTML={{__html: blogs.content.rendered}} />
+        </div>
+      );
+    })
 
     return (
       <div className="App">
-        <h1>Find a Brewery</h1>
-        <div className="search">
-        </div>
+        <h1>Finding Rubies Blog</h1>
+        <button>Click for 10 Most Recent Blog Posts!</button>
+
       </div>
     );
   }
 }
-
 
 
 export default App;
