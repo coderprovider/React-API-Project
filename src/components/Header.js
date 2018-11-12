e869c8f42e914d9d4118ce24343581ec0df35324
whitefury01 <white.pury1114@gmail.com>
2018-11-12 16:01:29 -0500

    fixed ul to use Link tags for react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = props =>
   <header>
@@ -8,9 +9,9 @@ const Header = props =>
     </div>
     <div>
       <ul className="main-nav">
-        <li><a href="/">Home</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/blog">Blog</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
       </ul>
     </div>
   </header>
